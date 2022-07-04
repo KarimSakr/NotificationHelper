5b0a3e1e355d3b50307cd592613eeec4bbb10642
KarimSakr <karimsakr000@gmail.com>
2022-07-04 14:57:23 +0300

    Initial Commit

---
+++ NotificationHelper/NotificationHelper.h
@@ -0,0 +1,18 @@
+//
+//  NotificationHelper.h
+//  NotificationHelper
+//
+//  Created by I-MagineWorks Mac on 04/07/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NotificationHelper.
+FOUNDATION_EXPORT double NotificationHelperVersionNumber;
+
+//! Project version string for NotificationHelper.
+FOUNDATION_EXPORT const unsigned char NotificationHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NotificationHelper/PublicHeader.h>
+
+
